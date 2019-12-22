86c8e4dcfaa346e05f646f985f3f58868bad2cea
Milan de Ruiter <milan.de.ruiter@nl.abnamro.com>
2019-12-22 14:16:36 +0100

    Update viewcontroller template

--- Templates/Examples/ViewController.swift
+++ Templates/Examples/ViewController.swift
@@ -5,7 +5,7 @@ import UIKit
 class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
     
     // MARK: - Properties
-    
+    var someProperty: SomePropertyType
     
     // MARK: - View lifecycle
     override func viewDidLoad() {
